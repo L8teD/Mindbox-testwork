db0c108b66fef79078f68cf155196d0a675dfc6e
Козырев Алексей <alex.kozyrev8@mail.ru>
2023-03-15 01:09:02 +0300

    Перенес .sln на уровень выше + добавил запрос для второго задания

---
+++ SQLQuery.sql
@@ -0,0 +1,7 @@
+﻿SELECT Products.Name as "Имя продукта", 
+	   Category.Name as "Имя категории"
+	   FROM ProductToCategory 
+	   LEFT JOIN Products
+	   ON ProductToCategory.ProductId = Products.Id
+	   LEFT JOIN Category
+	   ON ProductToCategory.CategoryId = Category.Id;
